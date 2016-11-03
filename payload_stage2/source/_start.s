6dab2531860aaa24b8f0943ff2f608cbd6571257
Gabriel Marcano <gabemarcano@yahoo.com>
2016-11-03 01:22:47 -0400

    Add start files and fix section order for stage2
    -Now using devKitARM's supplied start files.
    -Modified stage2's ld script to make sure that _start is first.
    -Stage2 _start now calls initialization functions for libc.

--- payload_stage2/source/_start.s
+++ payload_stage2/source/_start.s
@@ -4,11 +4,52 @@
 .global _start
 .extern flush_all_caches
 _start:
-_init:
+	@call libc initialization routines
+	adr r0, __libc_init_array_offset
+	ldr r1, [r0]
+	add r0, r1, r0
+	blx r0
 
-    ldr r0, =main
-    blx r0
+	adr r0, _fini_offset
+	ldr r1, [r0]
+	add r0, r1, r0
+	adr r1, atexit_offset
+	ldr r2, [r1]
+	add r1, r2, r1
+	blx r1
 
-.die:
-    b .die
+	adr r0, _init_offset
+	ldr r1, [r0]
+	add r0, r1, r0
+	blx r0
+
+	adr r0, main_offset
+	ldr r1,  [r0]
+	add r0, r1, r0
+	blx r0
+
+	adr r1, exit_offset
+	ldr r2, [r1]
+	add r1, r2, r1
+	blx r1
+
+	b . @die
+
+__libc_init_array_offset:
+.word __libc_init_array-.
+
+_fini_offset:
+.word _fini-.
+
+atexit_offset:
+.word atexit-.
+
+_init_offset:
+.word _init-.
+
+main_offset:
+.word main-.
+
+exit_offset:
+.word exit-.
 
